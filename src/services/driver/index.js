1871543b7142f1fbfafbe4ff6dfad8c2cba63182
Duc Cuong <cuongtu532@gmail.com>
2023-06-23 13:26:43 +0700

    Driver

---
+++ src/services/driver/index.js
@@ -0,0 +1,67 @@
+import axios from "axios";
+const URL = process.env.REACT_APP_SERVER;
+const END_POINTS = {
+    addDriver: '/api/Drivers',
+    updateDriver: '/api/Drivers',
+    getSingleDriver: '/api/Drivers',
+    getMultiDrivers: '/api/Drivers',
+    patchDriver: '/api/Drivers',
+    deleteDriver: '/api/Drivers'
+}
+
+export const addDriver = async (driver) => {
+    return await axios.post(END_POINTS.addDriver, {
+        Email: driver.email,
+        Code: driver.code,
+        FullName: code.fullName,
+        Gender: driver.gender,
+        IdCardNumber: driver.idCardNumber,
+        Address: driver.address,
+        PhoneNumber: driver.phoneNumber,
+        PersonalEmail: driver.personalEmail,
+        DateOfBirth: driver.dateOfBirth,
+        AvatarFile: driver.avatarFile,
+    })
+}
+
+export const updateDriver = async (driver) => {
+    return await axios.put(`${END_POINTS.updateDriver}/${driver.id}`, {
+        Email: driver.email,
+        Code: driver.code,
+        FullName: code.fullName,
+        Gender: driver.gender,
+        IdCardNumber: driver.idCardNumber,
+        Address: driver.address,
+        PhoneNumber: driver.phoneNumber,
+        PersonalEmail: driver.personalEmail,
+        DateOfBirth: driver.dateOfBirth,
+        AvatarFile: driver.avatarFile,
+    })
+}
+
+export const getSingleDriver = async (id) => {
+    return await axios.get(`${END_POINTS.getSingleDriver}/${id}`)
+}
+
+export const getMultiDrivers = async (data) => {
+    return await axios.get(END_POINTS.getMultiDrivers, {
+        params: {
+            Code: data.code,
+            Email: data.email,
+            PageIndex: code.pageIndex,
+            PageSize: data.pagesize,
+            OrderBy: data.OrderBy,
+            Direction: data.direction
+        }
+    })
+}
+
+export const patchDriver = async (data) => {
+    return await axios.patch(`${END_POINTS.patchDriver}/${data.id}`, {
+
+    })
+}
+
+export const deleteDriver = async (id) => {
+    return await axios.delete(`${END_POINTS.deleteDriver}/${id}`)
+}
